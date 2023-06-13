facf9529de4a9e88479cac7dc54fe9cf49b19900
azra1999 <127702672+azra1999@users.noreply.github.com>
2023-06-14 00:18:15 +0200

    Add files via upload

---
+++ data/znaksjevera_6.js
@@ -0,0 +1,1 @@
+var json_znaksjevera_6 = {"type":"FeatureCollection","name":"znaksjevera_6","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[16.531906044583984,44.519089159513349]}}]}
